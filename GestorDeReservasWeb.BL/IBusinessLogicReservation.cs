315ca22f10cdb06a2680c8793c1e68401f753184
marcoduarte21 <marco.duarte@ucr.ac.cr>
2024-03-11 19:52:40 -0600

    Creacion del metodo RegisterReservation

--- GestorDeReservasWeb.BL/IBusinessLogicReservation.cs
+++ GestorDeReservasWeb.BL/IBusinessLogicReservation.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace GestorDeReservasWeb.BL
 {
     public interface IBusinessLogicReservation
     {
 
+        public void RegisterReservation();
     }
 }
