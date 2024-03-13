d0983be70dc7b1e5beef67dbd1d5ddd49df4995b
marcoduarte21 <marco.duarte@ucr.ac.cr>
2024-03-12 19:44:22 -0600

    Implementacion de interfaz

--- GestorDeReservasWeb.BL/BusinessLogicReservation.cs
+++ GestorDeReservasWeb.BL/BusinessLogicReservation.cs
@@ -1,7 +1,42 @@
-﻿namespace GestorDeReservasWeb.BL
-{
-    public class BusinessLogicReservation
-    {
+﻿using GestorDeReservasWeb.Model;
 
+namespace GestorDeReservasWeb.BL
+{
+    public class BusinessLogicReservation : IBusinessLogicReservation
+    {
+        public void EditeLaReservacion(Reservacion reservacion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ElimineLaReservacion(Reservacion reservacion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Reservacion> GetAllReservacion()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Persona GetPersona(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Reservacion GetReservation(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterPerson(Persona persona)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterReservation(Reservacion reservacion)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
