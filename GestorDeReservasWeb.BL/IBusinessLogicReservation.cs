8d8ffeede22beae38a385495aec197e633864add
marcoduarte21 <marco.duarte@ucr.ac.cr>
2024-03-12 19:11:09 -0600

    Creacion de metodos para la BL, utilizados proximamente para API

--- GestorDeReservasWeb.BL/IBusinessLogicReservation.cs
+++ GestorDeReservasWeb.BL/IBusinessLogicReservation.cs
@@ -11,6 +11,12 @@ namespace GestorDeReservasWeb.BL
     public interface IBusinessLogicReservation
     {
 
-        public void RegisterReservation();
+        public void RegisterReservation(Model.Reservacion reservacion);
+        public void RegisterPerson(Model.Persona persona);
+        public List<Model.Reservacion> GetAllReservacion();
+        public void ElimineLaReservacion(Model.Reservacion reservacion);
+        public Model.Persona GetPersona(int id);
+        public Model.Reservacion GetReservation(int id);
+        
     }
 }
