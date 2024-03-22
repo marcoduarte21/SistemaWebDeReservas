89a8563f076b9bea3c5b64e8db95db532031076d
marcoduarte21 <marco.duarte@ucr.ac.cr>
2024-03-21 19:25:39 -0600

    Se agrego contenido a BL cliente

--- GestorDeReservasWeb.BL/Client/BLClient.cs
+++ GestorDeReservasWeb.BL/Client/BLClient.cs
@@ -9,14 +9,29 @@ namespace GestorDeReservasWeb.BL.Client
 {
     public class BLClient : IBLClient
     {
+        private DA.DbContexto DbContexto;
+        public BLClient(DA.DbContexto dbContexto)
+        {
+
+            DbContexto = dbContexto;
+
+        }
         public void AgendarCita(Appoinment appoinment)
         {
-            throw new NotImplementedException();
+            Model.Appoinment appoinmentAgendado = appoinment;
+            appoinment.statte = AppoinmentState.RESERVADA;
+            DbContexto.Appoinments.Update(appoinmentAgendado);
+            DbContexto.SaveChanges();
+           
         }
 
-        public List<Appoinment> GetListCitasProgramadas()
+        List<Appoinment> IBLClient.GetListCitasProgramadas(User user)
         {
-            throw new NotImplementedException();
+            var list = from appoinment in DbContexto.Appoinments
+                       where appoinment.statte == AppoinmentState.RESERVADA && appoinment.clientUser.userId == user.userId
+                       select appoinment;
+
+            return list.ToList();
         }
     }
 }
--- GestorDeReservasWeb.BL/Client/IBLClient.cs
+++ GestorDeReservasWeb.BL/Client/IBLClient.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GestorDeReservasWeb.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,7 @@ namespace GestorDeReservasWeb.BL.Client
     public interface IBLClient
     {
         public void AgendarCita(Model.Appoinment appoinment);
-        public List<Model.Appoinment> GetListCitasProgramadas();
+        public List<Model.Appoinment> GetListCitasProgramadas(User user);
 
     }
 }
