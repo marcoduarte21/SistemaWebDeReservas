9e1d823c22fe1e4279df09b8cef4b09595e3b0e3
marcoduarte21 <marco.duarte@ucr.ac.cr>
2024-03-24 16:25:15 -0600

    se cambio la logica de las listas programadas

--- GestorDeReservasWeb.BL/Employee/BLEmployee.cs
+++ GestorDeReservasWeb.BL/Employee/BLEmployee.cs
@@ -19,7 +19,7 @@ namespace GestorDeReservasWeb.BL.Employee
         public List<Appoinment> GetListCitasProgramadas(Model.Employee employee)
         {
             var list = from appoinment in DbContexto.Appoinments
-                       where appoinment.employeeInChange ==employee.employeeId
+                       where appoinment.employeeInChange ==employee.employeeId && appoinment.statte == AppoinmentState.RESERVADA
                        select appoinment;
 
             return list.ToList();
