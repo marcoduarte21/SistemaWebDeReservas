684f9b80ac26e412c5dd3fa6cdfa6fdde6c2e42f
marcoduarte21 <marco.duarte@ucr.ac.cr>
2024-03-24 14:56:46 -0600

    se agrego las dependencias y la cadena de conexion db

--- GestorDeReservasWeb.API/Program.cs
+++ GestorDeReservasWeb.API/Program.cs
@@ -1,3 +1,8 @@
+using GestorDeReservasWeb.BL.Admin;
+using GestorDeReservasWeb.BL.Client;
+using GestorDeReservasWeb.BL.Employee;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IBusinessLogicAdmin, IBusinessLogicAdmin>();
+builder.Services.AddScoped<IBLClient, IBLClient>();
+builder.Services.AddScoped<IBLEmployee, IBLEmployee>();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DbContext>(x => x.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
