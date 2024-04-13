5abdc9962f56807dfa68a41892b46c2fabc13aa4
marcoduarte21 <marco.duarte@ucr.ac.cr>
2024-04-13 00:26:04 -0600

    Se agrego un navBar

--- gestordereservasweb.ui/src/App.tsx
+++ gestordereservasweb.ui/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/navbar/Navbar';
 
 function App() {
   return (
-   <>Hola</>
+      <>
+          <NavBar />
+      </>
   );
 }
 
