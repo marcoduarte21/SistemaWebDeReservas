23e02f5094f67ed6fbc5d9a7b045e043099a5a8a
marcoduarte21 <marco.duarte@ucr.ac.cr>
2024-04-13 01:35:31 -0600

    Cmbios en app

--- gestordereservasweb.ui/src/App.tsx
+++ gestordereservasweb.ui/src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './components/navbar/Navbar';
+import NavBar from './components/navbar/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
   return (
       <>
           <NavBar />
--- gestordereservasweb.ui/src/components/navbar/NavBar.tsx
+++ gestordereservasweb.ui/src/components/navbar/NavBar.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 function NavBar() {
   return (
-    <p>Hello world!</p>
+      <>
+          <Navbar bg="dark" data-bs-theme="dark">
+              <Container>
+                  <Navbar.Brand href="#home">Reserva de citas</Navbar.Brand>
+                  <Nav className="me-auto">
+                      <Nav.Link href="#home">Home</Nav.Link>
+                      <Nav.Link href="#features">Users List</Nav.Link>
+                      <Nav.Link href="#pricing">Appoinments List</Nav.Link>
+                      <Nav.Link href="#pricing">Employees List</Nav.Link>
+                      <Nav.Link href="#pricing">Programar cita</Nav.Link>
+                      <Nav.Link href="#pricing">Citas programadas</Nav.Link>
+                  </Nav>
+              </Container>
+          </Navbar>
+      </>
   );
 }
 
