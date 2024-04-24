b5a8031467fb199c051efa8725a3155a8c8c400a
marcoduarte21 <marco.duarte@ucr.ac.cr>
2024-04-23 20:22:08 -0600

    added styles

--- gestordereservasweb.ui/src/views/Auth/loggin/Loggin.tsx
+++ gestordereservasweb.ui/src/views/Auth/loggin/Loggin.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Loggin.css';
 
 interface LogginProps {
@@ -7,17 +7,20 @@ interface LogginProps {
 
 export const Loggin: React.FC<LogginProps> = () => {
 
+    useEffect(() => {
+
+        document.title = 'Loggin';
+    },[])
+
     return (
 
         <div className="container">
             <div className="loggin-container">
                     <h1>Iniciar sesion</h1>
                 <div>
-                    <label htmlFor='username'>Nombre de usuario</label>
                     <input type='text' placeholder="nombre de usuario" id='username' name='username'/>
                 </div>
-                <div>
-                    <label htmlFor='clave'>Clave</label>
+                <div>                   
                     <input type='password' placeholder="Clave" id='clave' name='clave'/>
                 </div>
                 <div>
