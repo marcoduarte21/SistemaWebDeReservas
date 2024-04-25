f1a5630ebff61e8ab9047bcd5df73d3ad359aa60
marcoduarte21 <marco.duarte@ucr.ac.cr>
2024-04-25 15:46:17 -0600

    add functions in the files

--- gestordereservasweb.ui/src/hooks/useAuth.ts
+++ gestordereservasweb.ui/src/hooks/useAuth.ts
@@ -1,1 +1,28 @@
-console.log("Hello World!")
+import { useEffect } from "react";
+import { useUser } from "./useUser";
+import { User } from "./User"
+import { useLocalStorage } from "./useLocaleStorage";
+
+
+export const useAuth = () => {
+
+    const { user, addUser, removeUser, setUser } = useUser();
+    const { getItem } = useLocalStorage();
+
+    useEffect(() => {
+        const user = getItem("user");
+        if (user) {
+            addUser(JSON.parse(user));
+        }
+    }, [addUser, getItem]);
+
+    const login = (user: User) => {
+        addUser(user);
+    };
+
+    const logout = () => {
+        removeUser();
+    };
+
+    return { user, login, logout, setUser }
+};
--- gestordereservasweb.ui/src/hooks/useUser.ts
+++ gestordereservasweb.ui/src/hooks/useUser.ts
@@ -1,1 +1,22 @@
-console.log("Hello World!")
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { useLocalStorage } from "./useLocaleStorage";
+import { User } from "./User"
+
+export const useUser = () => {
+
+    const { user, setUser } = useContext(AuthContext);
+    const { setItem } = useLocalStorage();
+
+    const addUser = (user: User) => {
+        setUser(user);
+        setItem("user", JSON.stringify(user));
+    };
+
+    const removeUser = () => {
+        setUser(null);
+        setItem("user", "");
+    };
+
+    return { user, addUser, removeUser, setUser }
+};
