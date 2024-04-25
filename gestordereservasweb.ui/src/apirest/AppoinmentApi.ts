dacd281b21735ae3f66d1023f512a9b422da2c7f
marcoduarte21 <marco.duarte@ucr.ac.cr>
2024-04-25 15:08:30 -0600

    se quito espacio

--- gestordereservasweb.ui/src/apirest/AppoinmentApi.ts
+++ gestordereservasweb.ui/src/apirest/AppoinmentApi.ts
@@ -4,8 +4,6 @@ import { User } from '../models/User';
 const API = 'https://localhost:7232/api/Admin/';
 
 
-
-
 export const getUsers = async () => {
 
     return axios.get<User[]>(`${API}GetUsers`);
